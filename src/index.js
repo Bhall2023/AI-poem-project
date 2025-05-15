1f21695993e52303f47df19b56a3365cb485df75
Bhall2023 <147952158+Bhall2023@users.noreply.github.com>
2025-05-14 21:09:58 -0500

    updated user experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 function displayPoem(response){
-  console.log("poem generated", response);
-
   let poemText = response.data.answer; 
 
 new Typewriter("#poem", 
@@ -12,21 +10,18 @@ new Typewriter("#poem",
   });
 }
 
-
-
-
 function generatePoem(event) {
   event.preventDefault();
 
-  let instuctionsInput = document.querySelector("#user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7689111d9btfeef8e4e3cc0cba8be0o7";
   let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line romantic love poem. Make sure to follow the user instructions."; 
-  let prompt = `Generate a romatic love poem about ${instuctionsInput.value}`; 
+  let prompt = `Generate a romatic love poem about ${instructionsInput.value}`; 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
-console.log("Generating poem");
-console.log (`Prompt: ${prompt}`);
-console.log (`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a romantic love poem about ${instructionsInput.value}</div>`;
 
 axios.get(apiUrl).then(displayPoem);
 
